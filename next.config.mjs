cdf6144be8fe2c79aed5a9bcac6f5dfdd856de92
Cole Caccamise <colecaccamise@gmail.com>
2024-07-31 13:00:46 -0400

    chore: s3 config

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,46 @@
-import nextMDX from '@next/mdx';
+import nextMDX from "@next/mdx";
 
 const withMDX = nextMDX({
-	extension: /\.mdx?$/,
-	options: {
-		remarkPlugins: [],
-		rehypePlugins: [],
-	},
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 
-	images: {
-		domains: ['lh3.googleusercontent.com', 'localhost'],
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'yt3.googleusercontent.com',
-				port: '',
-				pathname: '/*',
-			},
-			{
-				protocol: 'https',
-				hostname: 'framerusercontent.com',
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'lh3.googleusercontent.com',
-				port: '',
-				pathname: '/****',
-			},
-		],
-	},
+  images: {
+    domains: ["lh3.googleusercontent.com", "localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "yt3.googleusercontent.com",
+        port: "",
+        pathname: "/*",
+      },
+      {
+        protocol: "https",
+        hostname: "framerusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/****",
+      },
+      {
+        protocol: "https",
+        hostname: "colecaccamise.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default withMDX(nextConfig);
