5175a4fd9037a6d2ebb86bf3eb2fd992f48dc86b
Cole Caccamise <colecaccamise@gmail.com>
2024-07-31 13:27:39 -0400

    fix: sort drops by position

--- lib/mdx/index.ts
+++ lib/mdx/index.ts
@@ -1,92 +1,103 @@
-'use server';
+"use server";
 
-import fs from 'fs';
-import path from 'path';
-import { compileMDX } from 'next-mdx-remote/rsc';
-import Video from '@/components/ui/video';
-import { notFound } from 'next/navigation';
-import { Drop, Letter, Stack } from '@/types/cms';
+import fs from "fs";
+import path from "path";
+import { compileMDX } from "next-mdx-remote/rsc";
+import Video from "@/components/ui/video";
+import { notFound } from "next/navigation";
+import { Drop, Letter, Stack } from "@/types/cms";
 
 type Frontmatter = Drop | Letter | Stack;
 
 const getItemsArray = (collection: string) => {
-	if (collection === 'drops') {
-		return [] as Drop[];
-	} else if (collection === 'letters') {
-		return [] as Letter[];
-	} else if (collection === 'stack') {
-		return [] as Stack[];
-	} else {
-		return [];
-	}
+  if (collection === "drops") {
+    return [] as Drop[];
+  } else if (collection === "letters") {
+    return [] as Letter[];
+  } else if (collection === "stack") {
+    return [] as Stack[];
+  } else {
+    return [];
+  }
 };
 
 // helpers
 export const getCollectionBySlug = async (slug: string, collection: string) => {
-	try {
-		const realSlug = slug.replace(/\.mdx$/, '');
+  try {
+    const realSlug = slug.replace(/\.mdx$/, "");
 
-		const rootDirectory = path.join(
-			process.cwd(),
-			'app',
-			'content',
-			collection
-		);
+    const rootDirectory = path.join(
+      process.cwd(),
+      "app",
+      "content",
+      collection,
+    );
 
-		const filePath = path.join(rootDirectory, `${realSlug}.mdx`);
+    const filePath = path.join(rootDirectory, `${realSlug}.mdx`);
 
-		const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' });
+    const fileContent = fs.readFileSync(filePath, { encoding: "utf8" });
 
-		const { frontmatter, content }: { frontmatter: Frontmatter; content: any } =
-			await compileMDX({
-				source: fileContent,
-				options: { parseFrontmatter: true },
-				components: { Video },
-			});
+    const { frontmatter, content }: { frontmatter: Frontmatter; content: any } =
+      await compileMDX({
+        source: fileContent,
+        options: { parseFrontmatter: true },
+        components: { Video },
+      });
 
-		return { meta: { ...frontmatter, slug: realSlug }, content };
-	} catch (error) {
-		return notFound();
-	}
+    return { meta: { ...frontmatter, slug: realSlug }, content };
+  } catch (error) {
+    return notFound();
+  }
 };
 
 export const getAllCollectionMeta = async (
-	collection: string,
-	limit?: number,
-	exclude?: string
+  collection: string,
+  limit?: number,
+  exclude?: string,
 ) => {
-	try {
-		const rootDirectory = path.join(
-			process.cwd(),
-			'app',
-			'content',
-			collection
-		);
+  try {
+    const rootDirectory = path.join(
+      process.cwd(),
+      "app",
+      "content",
+      collection,
+    );
 
-		const files = fs.readdirSync(rootDirectory);
+    const files = fs.readdirSync(rootDirectory);
 
-		let items: Drop[] | Stack[] | Letter[] = getItemsArray(collection);
+    let items: Drop[] | Stack[] | Letter[] = getItemsArray(collection);
 
-		const effectiveLimit = limit ?? files.length;
-		let count = 0;
+    const effectiveLimit = limit ?? files.length;
+    let count = 0;
 
-		for (let i = 0; i < files.length && count < effectiveLimit; i++) {
-			const file = path.parse(files[i]).name;
+    for (let i = 0; i < files.length && count < effectiveLimit; i++) {
+      const file = path.parse(files[i]).name;
 
-			// Skip the file if it matches the exclude slug
-			if (exclude && file === exclude) {
-				continue;
-			}
+      // Skip the file if it matches the exclude slug
+      if (exclude && file === exclude) {
+        continue;
+      }
 
-			const { meta }: { meta: Drop | Letter | Stack } =
-				await getCollectionBySlug(`${file}`, collection);
-			items.push(meta as any);
+      const { meta }: { meta: Drop | Letter | Stack } =
+        await getCollectionBySlug(`${file}`, collection);
+      items.push(meta as any);
 
-			count++;
-		}
+      count++;
+    }
 
-		return items;
-	} catch (error) {
-		return notFound();
-	}
+    if (collection === "drops") {
+      items.sort((a: Drop, b: Drop) => {
+        // If a.position is undefined, push it to the end by setting it to a higher value (e.g., Infinity)
+        const posA = a.position !== undefined ? a.position : Infinity;
+        // If b.position is undefined, push it to the end by setting it to a higher value (e.g., Infinity)
+        const posB = b.position !== undefined ? b.position : Infinity;
+
+        return posA - posB;
+      });
+    }
+
+    return items;
+  } catch (error) {
+    return notFound();
+  }
 };
--- types/cms/index.ts
+++ types/cms/index.ts
@@ -3,33 +3,34 @@ export type Params = { slug: string };
 export type Meta = { slug: string; name?: string; title?: string };
 
 export type Drop = {
-	slug?: string;
-	category?: string;
-	seoDescription?: string;
-	price?: number;
-	oldPrice?: number;
-	name?: string;
-	lemonSqueezyLink?: string;
-	demoLink?: string;
-	thumbnailImage?: string;
-	image1?: string;
-	image2?: string;
-	image3?: string;
-	image4?: string;
-	image5?: string;
-	image6?: string;
+  slug?: string;
+  category?: string;
+  seoDescription?: string;
+  price?: number;
+  oldPrice?: number;
+  name?: string;
+  lemonSqueezyLink?: string;
+  demoLink?: string;
+  thumbnailImage?: string;
+  image1?: string;
+  image2?: string;
+  image3?: string;
+  image4?: string;
+  image5?: string;
+  image6?: string;
+  position?: number;
 };
 
 export type Letter = {
-	slug?: string;
-	title?: string;
-	published?: string;
-	description?: string;
+  slug?: string;
+  title?: string;
+  published?: string;
+  description?: string;
 };
 
 export type Stack = {
-	slug?: string;
-	title?: string;
-	description?: string;
-	seoDescription?: string;
+  slug?: string;
+  title?: string;
+  description?: string;
+  seoDescription?: string;
 };
