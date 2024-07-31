d2deacef449b629939ed121af6c6af5ed08da1c8
Cole Caccamise <colecaccamise@gmail.com>
2024-07-31 13:05:25 -0400

    feat: add custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex h-full flex-col gap-4">
+      <h1>Yikes, that&apos; a 404.</h1>
+      <p>
+        This is embarassing, but I couldn&apos;t find what you&apos;re looking
+        for.
+      </p>
+      <Link href="/">Return Home</Link>
+    </div>
+  );
+}
