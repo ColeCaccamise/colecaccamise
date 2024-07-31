a8412f4abab3e4e8caab3e2f217818839e3db6b4
Cole Caccamise <colecaccamise@gmail.com>
2024-07-31 13:13:28 -0400

    fix: add additional s3 hostname

--- next.config.mjs
+++ next.config.mjs
@@ -39,6 +39,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "colecaccamise.s3.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
