0659cb4c07f5743adc42ee4a8f48c8fc68e42cc5
Cole Caccamise <colecaccamise@gmail.com>
2024-08-01 17:11:13 -0400

    style: adjust listicle gap

--- components/ui/list/list-container.tsx
+++ components/ui/list/list-container.tsx
@@ -1,19 +1,19 @@
 export default function ListContainer({
-	children,
-	title,
-	description,
+  children,
+  title,
+  description,
 }: {
-	children: React.ReactNode;
-	title: string;
-	description: string;
+  children: React.ReactNode;
+  title: string;
+  description: string;
 }) {
-	return (
-		<div className='flex flex-col gap-4'>
-			<div className='pb-3 border-b border-ui-component-default'>
-				<span className='font-medium'>{title}</span>
-				<p>{description}</p>
-			</div>
-			{children}
-		</div>
-	);
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="border-b border-ui-component-default pb-3">
+        <span className="font-medium">{title}</span>
+        <p>{description}</p>
+      </div>
+      {children}
+    </div>
+  );
 }
--- components/ui/list/list-item.tsx
+++ components/ui/list/list-item.tsx
@@ -1,66 +1,60 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import ListDescription from './list-description';
+import Link from "next/link";
+import Image from "next/image";
+import ListDescription from "./list-description";
 
-type AllowedKinds = 'drops' | 'letters' | 'stack' | 'ventures';
+type AllowedKinds = "drops" | "letters" | "stack" | "ventures";
 
 export default function ListItem({
-	item,
-	kind,
+  item,
+  kind,
 }: {
-	item: any;
-	kind: AllowedKinds;
+  item: any;
+  kind: AllowedKinds;
 }) {
-	const url = item?.url || (item?.slug && `/${kind}/${item.slug}`) || '';
+  const url = item?.url || (item?.slug && `/${kind}/${item.slug}`) || "";
 
-	const pattern = /^(?:[a-z]+:)?\/\//i;
-	const isNonRelativePath = pattern.test(url);
+  const pattern = /^(?:[a-z]+:)?\/\//i;
+  const isNonRelativePath = pattern.test(url);
 
-	const className = `flex py-6 gap-4 items-center no-underline ${
-		url
-			? 'hover:bg-sidebar-bg transition-all duration-200 rounded-md hover:border-ui-component-default hover:px-3 hover:-mx-3'
-			: 'select-none cursor-wait opacity-80'
-	}`;
+  const className = `flex py-6 gap-4 items-center no-underline ${
+    url
+      ? "hover:bg-sidebar-bg transition-all duration-200 rounded-md hover:border-ui-component-default hover:px-3 hover:-mx-3"
+      : "select-none cursor-wait opacity-80"
+  }`;
 
-	return url ? (
-		<Link
-			href={url}
-			target={isNonRelativePath ? '_blank' : ''}
-			className={className}
-		>
-			{item.thumbnailImage && (
-				<div className='w-16 h-16 overflow-hidden'>
-					<Image
-						className='rounded-lg object-cover h-full'
-						src={item.thumbnailImage}
-						alt={item.name || item.title}
-						width={512}
-						height={512}
-					/>
-				</div>
-			)}
-			<ListDescription
-				item={item}
-				kind={kind}
-			/>
-		</Link>
-	) : (
-		<span className={className}>
-			{item.thumbnailImage && (
-				<div className='w-16 h-16 overflow-hidden'>
-					<Image
-						className='rounded-lg object-cover h-full'
-						src={item.thumbnailImage}
-						alt={item.name || item.title}
-						width={512}
-						height={512}
-					/>
-				</div>
-			)}
-			<ListDescription
-				item={item}
-				kind={kind}
-			/>
-		</span>
-	);
+  return url ? (
+    <Link
+      href={url}
+      target={isNonRelativePath ? "_blank" : ""}
+      className={className}
+    >
+      {item.thumbnailImage && (
+        <div className="h-16 w-16 overflow-hidden">
+          <Image
+            className="h-full rounded-lg object-cover"
+            src={item.thumbnailImage}
+            alt={item.name || item.title}
+            width={512}
+            height={512}
+          />
+        </div>
+      )}
+      <ListDescription item={item} kind={kind} />
+    </Link>
+  ) : (
+    <span className={className}>
+      {item.thumbnailImage && (
+        <div className="h-16 w-16 overflow-hidden">
+          <Image
+            className="h-full rounded-lg object-cover"
+            src={item.thumbnailImage}
+            alt={item.name || item.title}
+            width={512}
+            height={512}
+          />
+        </div>
+      )}
+      <ListDescription item={item} kind={kind} />
+    </span>
+  );
 }
