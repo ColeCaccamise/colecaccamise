f78669ffef0fd8edb7f947d0bab08302fc942d68
Cole Caccamise <colecaccamise@gmail.com>
2024-08-01 17:09:58 -0400

    fix: fix bad padding on newsletter signup

--- components/ui/newsletter-signup.tsx
+++ components/ui/newsletter-signup.tsx
@@ -1,101 +1,102 @@
-'use client';
+"use client";
 
-import Input from './input';
-import Button from './button';
-import { useState } from 'react';
-import toast from '@/utils/toast';
-import { isValidEmail } from '@/lib/validation';
-import { subscribeToForm } from '@/lib/convertkit';
-import { usePlausible } from 'next-plausible';
+import Input from "./input";
+import Button from "./button";
+import { useState } from "react";
+import toast from "@/utils/toast";
+import { isValidEmail } from "@/lib/validation";
+import { subscribeToForm } from "@/lib/convertkit";
+import { usePlausible } from "next-plausible";
 
 export default function NewsletterSignup({
-	title,
-	formId = '5584232',
-	description,
-	cta = 'Join For Free',
-	location = 'Home',
-	lemonSqueezySignup = false,
+  title,
+  formId = "5584232",
+  description,
+  cta = "Join For Free",
+  location = "Home",
+  lemonSqueezySignup = false,
 }: {
-	title?: string;
-	formId?: string;
-	description?: string;
-	cta?: string;
-	location?: string;
-	lemonSqueezySignup?: boolean;
+  title?: string;
+  formId?: string;
+  description?: string;
+  cta?: string;
+  location?: string;
+  lemonSqueezySignup?: boolean;
 }) {
-	const [email, setEmail] = useState('');
-	const [formSubmitting, setFormSubmitting] = useState(false);
-	const plausible = usePlausible();
+  const [email, setEmail] = useState("");
+  const [formSubmitting, setFormSubmitting] = useState(false);
+  const plausible = usePlausible();
 
-	// TODO: add customers to lemon squeezy discount form, then use resend to send them a programatically gnerated discount code -- implement convertkit unsubscribes
-	// You are receiving this message because you purchased/downloaded a product from Cole Caccamise.
+  // TODO: add customers to lemon squeezy discount form, then use resend to send them a programatically gnerated discount code -- implement convertkit unsubscribes
+  // You are receiving this message because you purchased/downloaded a product from Cole Caccamise.
 
-	const handleSignup = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
+  const handleSignup = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-		if (!email) {
-			return toast('Email is required.', 'error');
-		}
+    if (!email) {
+      return toast("Email is required.", "error");
+    }
 
-		if (!isValidEmail(email)) {
-			setEmail('');
-			return toast('A valid email address is required.', 'error');
-		}
+    if (!isValidEmail(email)) {
+      setEmail("");
+      return toast("A valid email address is required.", "error");
+    }
 
-		if (!formId) return toast('Newsletter not found.', 'error');
+    if (!formId) return toast("Newsletter not found.", "error");
 
-		setFormSubmitting(true);
+    setFormSubmitting(true);
 
-		subscribeToForm(formId, email)
-			.then((status) => {
-				if (status == 'inactive') {
-					toast('Check your email - I just need you to verify!', 'success');
-					plausible('Newsletter Signup', { props: { location: location } });
-				} else {
-					toast("You're subscribed!", 'success');
-				}
+    subscribeToForm(formId, email)
+      .then((status) => {
+        if (status == "inactive") {
+          toast("Check your email - I just need you to verify!", "success");
+          plausible("Newsletter Signup", { props: { location: location } });
+        } else {
+          toast("You're subscribed!", "success");
+        }
 
-				setEmail('');
-				setFormSubmitting(false);
-			})
-			.catch((err) => {
-				toast(err.message, 'error');
+        setEmail("");
+        setFormSubmitting(false);
+      })
+      .catch((err) => {
+        toast(err.message, "error");
 
-				setEmail('');
-				setFormSubmitting(false);
-			});
-	};
+        setEmail("");
+        setFormSubmitting(false);
+      });
+  };
 
-	return (
-		<div className='flex flex-col gap-4'>
-			{title && (
-				<div className='flex flex-col gap-2'>
-					<span className='font-medium'>{title}</span>
-					<p>{description}</p>
-				</div>
-			)}
+  return (
+    <div className="flex flex-col gap-4">
+      {title && (
+        <div className="flex flex-col gap-2">
+          {" "}
+          <span className="font-medium">{title}</span>
+          <p>{description}</p>{" "}
+        </div>
+      )}
 
-			<form
-				onSubmit={handleSignup}
-				className='flex bg-ui-component-default border border-borders-non-interactive rounded-md p-2 pl-4'
-			>
-				<Input
-					variant='unstyled'
-					className='bg-transparent w-full'
-					type='email'
-					value={email}
-					handleChange={(e) => setEmail(e.target.value)}
-					placeholder='Your email'
-					required
-				/>
-				<Button
-					type='submit'
-					className='font-medium bg-primary '
-					loading={formSubmitting}
-				>
-					{cta}
-				</Button>
-			</form>
-		</div>
-	);
+      <form
+        onSubmit={handleSignup}
+        className="flex flex-col gap-4 rounded-md border-0 border-borders-non-interactive bg-transparent md:flex-row md:gap-0 md:border md:bg-ui-component-default"
+      >
+        <Input
+          variant="unstyled"
+          className="flex w-full items-center justify-between overflow-hidden rounded-lg border border-subtle-borders-interactive bg-transparent px-4 py-3 hover:border-stronger-borders-interactive-focus-rings md:border-none md:bg-app-bg md:px-0 dark:bg-ui-component-default"
+          type="email"
+          value={email}
+          handleChange={(e) => setEmail(e.target.value)}
+          placeholder="Your email"
+          required
+        />
+        <Button
+          type="submit"
+          className="bg-primary font-medium"
+          loading={formSubmitting}
+        >
+          {cta}
+        </Button>
+      </form>
+    </div>
+  );
 }
