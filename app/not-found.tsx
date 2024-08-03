72d06017a45e9621c1f939bc2332a9e36b59ec25
Cole Caccamise <colecaccamise@gmail.com>
2024-08-03 17:58:34 -0400

    fix: fix typo on 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="flex h-full flex-col gap-4">
-      <h1>Yikes, that&apos; a 404.</h1>
+      <h1>Yikes, that&apos;s a 404.</h1>
       <p>
         This is embarassing, but I couldn&apos;t find what you&apos;re looking
         for.
