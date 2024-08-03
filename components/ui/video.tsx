f4d91856ee5476e5d789a7ce010e16ab397c070d
Cole Caccamise <colecaccamise@gmail.com>
2024-08-03 18:37:33 -0400

    fix: update video component to support non-youtube videos

--- components/ui/video.tsx
+++ components/ui/video.tsx
@@ -1,28 +1,27 @@
-import { YouTubeEmbed } from '@next/third-parties/google';
+import { YouTubeEmbed } from "@next/third-parties/google";
+import Video from "next-video";
 
-export default function Video({ url }: { url: string }) {
-	if (url === undefined) {
-		return null;
-	}
+export default function VideoPlayer({ url }: { url: string }) {
+  console.log(url);
+  if (url === undefined) {
+    return null;
+  }
 
-	// return null if not youtube.com or youtu.be
-	if (
-		typeof url !== 'string' ||
-		(!url.includes('youtube.com') && !url.includes('youtu.be'))
-	) {
-		return null;
-	}
+  // return null if not youtube.com or youtu.be
+  if (
+    typeof url !== "string" ||
+    (!url.includes("youtube.com") && !url.includes("youtu.be"))
+  ) {
+    return <Video src={url} accentColor="#5e69d1" loop />;
+  } else {
+    const videoId = url.includes("youtu.be")
+      ? url.split("youtu.be/")[1]
+      : url.split("v=")[1];
 
-	const videoId = url.includes('youtu.be')
-		? url.split('youtu.be/')[1]
-		: url.split('v=')[1];
-
-	return (
-		<div className='rounded-md overflow-hidden'>
-			<YouTubeEmbed
-				videoid={videoId}
-				params='web-share;'
-			/>
-		</div>
-	);
+    return (
+      <div className="overflow-hidden rounded-md">
+        <YouTubeEmbed videoid={videoId} params="web-share;" />
+      </div>
+    );
+  }
 }
