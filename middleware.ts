be39003693b79b2c05072b3f70eb3f344c5113b9
Cole Caccamise <colecaccamise@gmail.com>
2024-08-05 15:44:15 -0400

    fix: comment out middleware

--- middleware.ts
+++ middleware.ts
@@ -1,62 +1,62 @@
-import { NextResponse, NextRequest } from 'next/server';
-import { get } from '@vercel/edge-config';
-import { updateSession } from '@/utils/supabase/middleware';
-import { isUserAuthenticated } from '@/utils/auth';
-import { isEmpty } from './lib/validation';
+import { NextResponse, NextRequest } from "next/server";
+import { get } from "@vercel/edge-config";
+import { updateSession } from "@/utils/supabase/middleware";
+import { isUserAuthenticated } from "@/utils/auth";
+import { isEmpty } from "./lib/validation";
 
 type RedirectEntry = {
-	destination: string;
-	permanent: boolean;
+  destination: string;
+  permanent: boolean;
 };
 
 interface Redirects {
-	[pathname: string]: RedirectEntry;
+  [pathname: string]: RedirectEntry;
 }
 
 export async function middleware(request: NextRequest) {
-	await updateSession(request);
+  // await updateSession(request);
 
-	const pathname: string = request.nextUrl.pathname;
+  // const pathname: string = request.nextUrl.pathname;
 
-	const redirects: Redirects = (await get('redirects')) || {};
-	const redirectEntry: RedirectEntry = redirects[pathname];
+  // const redirects: Redirects = (await get('redirects')) || {};
+  // const redirectEntry: RedirectEntry = redirects[pathname];
 
-	if (!isEmpty(redirects) && redirectEntry) {
-		const statusCode = redirectEntry?.permanent ? 308 : 307;
-		return NextResponse.redirect(redirectEntry.destination, statusCode);
-	}
+  // if (!isEmpty(redirects) && redirectEntry) {
+  // 	const statusCode = redirectEntry?.permanent ? 308 : 307;
+  // 	return NextResponse.redirect(redirectEntry.destination, statusCode);
+  // }
 
-	const user = await isUserAuthenticated();
+  const user = await isUserAuthenticated();
 
-	// Check if the current path is an auth route
-	const isAuthRoute = pathname.startsWith('/admin/auth/');
+  // Check if the current path is an auth route
+  const isAuthRoute = pathname.startsWith("/admin/auth/");
 
-	// Check if the current path is in the admin directory
-	const isAdminRoute = pathname.startsWith('/admin');
+  // Check if the current path is in the admin directory
+  const isAdminRoute = pathname.startsWith("/admin");
 
-	if (user && isAdminRoute) {
-		// Redirect signed-in users from /admin/login to /admin
-		if (pathname === '/admin/login') {
-			return Response.redirect(new URL('/admin', request.url));
-		}
-		// Allow access to all other routes for signed-in users
-		return;
-	}
+  if (user && isAdminRoute) {
+    // Redirect signed-in users from /admin/login to /admin
+    if (pathname === "/admin/login") {
+      return Response.redirect(new URL("/admin", request.url));
+    }
+    // Allow access to all other routes for signed-in users
+    return;
+  }
 
-	// Allow access to auth routes and /admin/login even if not signed in
-	if (isAuthRoute || pathname === '/admin/login') {
-		return;
-	}
+  // Allow access to auth routes and /admin/login even if not signed in
+  if (isAuthRoute || pathname === "/admin/login") {
+    return;
+  }
 
-	// Redirect non-signed-in users trying to access any other admin routes to /admin/login
-	if (isAdminRoute) {
-		return Response.redirect(new URL('/admin/login', request.url));
-	}
+  // Redirect non-signed-in users trying to access any other admin routes to /admin/login
+  if (isAdminRoute) {
+    return Response.redirect(new URL("/admin/login", request.url));
+  }
 
-	// Allow access to all other routes
-	return;
+  // Allow access to all other routes
+  return;
 }
 
 export const config = {
-	matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
