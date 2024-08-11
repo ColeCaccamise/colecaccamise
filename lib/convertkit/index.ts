3e8f2cff99c78ee496c5f48e106b4448f63b9ec9
Cole Caccamise <colecaccamise@gmail.com>
2024-08-11 08:28:10 -0400

    feat: update new video

--- lib/convertkit/index.ts
+++ lib/convertkit/index.ts
@@ -1,31 +1,31 @@
-'use server';
+"use server";
 
-import axios from 'axios';
+import axios from "axios";
 
 type SubscribeData = {
-	api_key: string;
-	email: string;
+  api_key: string;
+  email: string;
 };
 
 export const subscribeToForm = async (formId: string, email: string) => {
-	const CONVERTKIT_API_KEY = process.env.CONVERTKIT_API_KEY;
+  const CONVERTKIT_API_KEY = process.env.CONVERTKIT_API_KEY;
 
-	const url = `https://api.convertkit.com/v3/forms/${formId}/subscribe`;
+  const url = `https://api.convertkit.com/v3/forms/${formId}/subscribe`;
 
-	const data: SubscribeData = {
-		api_key: CONVERTKIT_API_KEY || '',
-		email: email,
-	};
+  const data: SubscribeData = {
+    api_key: CONVERTKIT_API_KEY || "",
+    email: email,
+  };
 
-	try {
-		const response = await axios.post(url, data, {
-			headers: {
-				'Content-Type': 'application/json; charset=utf-8',
-			},
-		});
+  try {
+    const response = await axios.post(url, data, {
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+    });
 
-		return response.data.subscription.state;
-	} catch (error) {
-		throw new Error('I could not sign you up. Please try again.');
-	}
+    return response.data.subscription.state;
+  } catch (error) {
+    throw new Error("I could not sign you up. Please try again.");
+  }
 };
