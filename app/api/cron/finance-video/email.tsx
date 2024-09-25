cb0e3087e66d91184d9698b9e168c4e7c8bd7b87
Cole Caccamise <colecaccamise@gmail.com>
2024-09-25 18:31:12 -0400

    fix: add line breaks to cron email

--- app/api/cron/finance-video/email.tsx
+++ app/api/cron/finance-video/email.tsx
@@ -6,13 +6,18 @@ export default function FinanceVideoEmail() {
         You gotta keep this stuff top of mind. I dropped a few good ones for
         you.
       </p>
+
       <p>
         <a href="https://www.youtube.com/watch?v=RTQMMt1mwZw">75/15/10 Rule</a>
+      </p>
 
+      <p>
         <a href="https://www.youtube.com/watch?v=AlX9PhlQxP4">
           How to think like a rich person
         </a>
+      </p>
 
+      <p>
         <a href="https://www.youtube.com/watch?v=Tc8aMtN-B20">
           The math to become a millionaire
         </a>
