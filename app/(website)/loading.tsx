5758dce79a19b29d538704b30ccf50a17b89ff20
Cole Caccamise <colecaccamise@gmail.com>
2024-10-20 06:48:20 -0400

    chore: reposition loader

--- app/(website)/loading.tsx
+++ app/(website)/loading.tsx
@@ -1,6 +1,10 @@
-import Spinner from '@/components/ui/spinner';
+import Spinner from "@/components/ui/spinner";
 
 export default function Loading() {
-	// Or a custom loading skeleton component
-	return <Spinner variant='light' />;
+  // Or a custom loading skeleton component
+  return (
+    <div className="flex h-screen items-start justify-center py-20">
+      <Spinner variant="light" />
+    </div>
+  );
 }
