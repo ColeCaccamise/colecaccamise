559b815eb778f7557a9a576b2e95fc52277146fb
Cole Caccamise <colecaccamise@gmail.com>
2025-01-18 20:11:17 -0500

    feat: add tools to vault

--- app/(website)/vault/page.tsx
+++ app/(website)/vault/page.tsx
@@ -7,98 +7,84 @@ export const metadata = {
   description: "Links to valuable resources I've come across.",
 };
 
+function getYoutubeImage(url: string) {
+  const videoId = url.split("v=")[1];
+  return `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`;
+}
+
 export default async function VaultPage() {
   const vaultItems = [
     {
-      title: "Speak Like A Leader: Make People Respect You",
-      description: "A great watch with Lelia Hormozi and Simon Quibb.",
-      url: "https://www.youtube.com/watch?v=fABX411MixY",
-      image: await getOpenGraphImage(
-        "https://www.youtube.com/watch?v=fABX411MixY",
-      ),
-      tags: ["videos"],
+      title: "Ghostty",
+      description: "A fast terminal emulator for macOS.",
+      url: "https://ghostty.org/",
+      image: "https://ghostty.org/social-share-card.jpg",
+      tags: ["tools"],
     },
     {
-      title:
-        "Jason Fried challenges your thinking on fundraising, goals, growth, and more",
-      description: "The case for profitability over growth.",
-      url: "https://www.youtube.com/watch?v=dAnF0tk0di8",
-      image: await getOpenGraphImage(
-        "https://www.youtube.com/watch?v=dAnF0tk0di8",
-      ),
-      tags: ["videos"],
+      title: "Zen Browser",
+      description: "A sleak, firefox-based browser.",
+      url: "https://zen-browser.app/",
+      image: "https://zen-browser.app/share-pic.png",
+      tags: ["tools"],
+    },
+    {
+      title: "Yaak",
+      description: "A local-first API client.",
+      url: "https://yaak.app/",
+      image: "https://yaak.app/static/og.png",
+      tags: ["tools"],
     },
     {
       title: "Dub.co",
       description: "Create powerful short links.",
       url: "https://refer.dub.co/colecaccamise",
-      image: await getOpenGraphImage("https://dub.co/"),
+      image: "https://assets.dub.co/thumbnail.jpg",
       tags: ["companies"],
     },
     {
       title: "Railway",
       description: "Easily ship backend apps/services.",
       url: "https://caccamise.link/railway",
-      image: await getOpenGraphImage("https://railway.app"),
+      image: "https://railway.app/og.png?v=2",
       tags: ["companies"],
     },
     {
       title: "Resend",
       description: "Email for developers.",
       url: "https://resend.com/",
-      image: await getOpenGraphImage("https://resend.com"),
+      image: "https://resend.com/static/cover.png",
       tags: ["companies"],
     },
     {
       title: "Cursor",
       description: "Fast, AI-powered code editor.",
       url: "https://cursor.com",
-      image: await getOpenGraphImage("https://cursor.com"),
+      image: "https://www.cursor.com/opengraph-image.png",
       tags: ["companies"],
     },
     {
       title: "Claude",
       description: "My favorite LLM for programming and idea generation.",
       url: "https://claude.ai/new",
-      image: await getOpenGraphImage("https://claude.ai/new"),
+      image: "https://claude.ai/images/claude_ogimage.png",
       tags: ["companies"],
     },
     {
       title: "Superhuman",
       description: "The fastest and prettiest email client.",
       url: "https://caccamise.link/superhuman",
-      image: await getOpenGraphImage("https://superhuman.com"),
+      image:
+        "https://framerusercontent.com/images/kVLfqsm3HAwPB0Y6h08ymuwuE.jpg",
       tags: ["companies"],
     },
     {
       title: "Raycast",
       description: "The best Mac productivity tool.",
       url: "https://www.raycast.com/?via=cole",
-      image: await getOpenGraphImage("https://www.raycast.com/"),
+      image: "https://www.raycast.com/opengraph-image-pwu6ef.png",
       tags: ["companies"],
     },
-    {
-      title: "Warp",
-      description: "AI-powered terminal.",
-      url: "https://caccamise.link/warp",
-      image: await getOpenGraphImage("https://warp.dev"),
-      tags: ["companies"],
-    },
-    {
-      title: "Digital Ocean",
-      description:
-        "Simple and afforadble VPS hosting. What I use for some of my smaller projects.",
-      url: "https://caccamise.link/digitalocean",
-      image: await getOpenGraphImage("https://digitalocean.com"),
-      tags: ["companies"],
-    },
-    {
-      title: "Kit.com",
-      description: "Simple newsletters.",
-      url: "https://kit.com",
-      image: await getOpenGraphImage("https://kit.com"),
-      tags: ["companies"],
-    }
   ];
 
   return (
@@ -106,8 +92,8 @@ export default async function VaultPage() {
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-medium">Vault</h1>
         <p>
-          Links to valuable resources I&apos;ve come across. More added all the
-          time.
+          Links to valuable tools and resources I&apos;ve come across. More
+          added all the time.
         </p>
       </div>
 
